490fbef8cbbb6cc0f2bc271e5100fdd81172c63b
cabobadilla <cbobadilla.dsoft@gmail.com>
2025-01-19 10:11:24 -0300

    V.101 clean

--- src/utils/promptTemplates.js
+++
@@ -1,6 +0,0 @@
-export const generateMusicPrompt = (genre, mood) => {
-  return `Suggest 10 ${genre} songs that evoke a ${mood} mood. ` +
-    'For each song provide the following JSON structure: ' +
-    '{"title": "song name", "artist": "artist name", "year": "release year"}. ' +
-    'Return only a JSON array with these objects.';
-}; 
